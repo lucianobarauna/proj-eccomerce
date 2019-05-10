cd23a5d5b789e4bef8ec33998b443f0750f9eb36
Luciano Baraúna <baraunaluciano@gmail.com>
2019-05-10 18:22:46 -0300

    criando class template e templates

---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,63 @@
+<?php 
+
+// Template padrão do projeto.
+namespace Hcode;
+
+use Rain\Tpl;
+
+class Page
+{
+    private $tpl;
+    private $options = [];
+    private $defaults = [
+        "data" => []
+    ];
+
+    public function __construct($opts = array()) {
+        // $opts para a classe
+        // config rain tpl
+        
+        // array_merge - sobrescreve o primeiro array com o segundo.
+        $this->options = array_merge($this->defaults, $opts);
+
+        // $_SERVER["DOCUMENT_ROOT"] - traz o caminho do root
+        $config = array(
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/", # diretório de templates
+            "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/", # diretório de cache do template
+            "debug"         => false // set to false to improve the speed
+        );
+
+        Tpl::configure( $config );
+
+        // Criando o template.
+        $this->tpl = new Tpl;
+        $this->setData($this->options["data"]);
+
+        // Montando o cabeçalho header em todos os templates.
+        $this->tpl->draw("header");
+
+    }
+
+    private function setData($data = array())
+    {
+        // Pegando as variáveis e passando para o template
+        foreach ($data as $key => $value) {
+            $this->tpl->assign($key, $value);
+        }
+    }
+
+    public function setTpl($name, $data = array(), $returnHTML = false)
+    {
+        // é o argumento que diz se vamos ter que jogar o html par ao cliente.
+        $this->setData($data);
+        return $this->tpl->draw($name, $returnHTML);
+    }
+    
+    public function __destruct() {
+        // Quando sair do cache do php é que inserimos o footer. Aqui vai conter
+        // os arquivos JS.
+        $this->tpl->draw("footer");
+    }
+}
+
+
