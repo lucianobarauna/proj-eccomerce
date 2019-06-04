c76dac2121474fb8fac0a82e033bd0a4e99e2f5f
Luciano Baraúna <baraunaluciano@gmail.com>
2019-06-04 19:37:36 -0300

    finalizado aula de crud usuários

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ $app->get('/', function() {
     $page->setTpl("index");
 });
 
+//  Admin
 $app->get('/admin', function() {
     // Vefica se o usuário está logado.
     User::verifyLogin();
@@ -45,5 +46,96 @@ $app->get('/admin/logout', function() {
    header("Location: /admin/login");
    exit;
 });
+
+//  Users
+$app->get('/admin/users', function() {
+    User::verifyLogin();
+
+    $users = User::listAll();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("users", array(
+        "users"=>$users
+    ));
+});
+
+$app->get('/admin/users/create', function() {
+    User::verifyLogin();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("users-create");
+});
+
+$app->get('/admin/users/:iduser/delete', function($iduser) {
+    User::verifyLogin();
+
+    $user =  new User();
+
+    $user->get((int)$iduser);
+
+    $user->delete();
+
+    header('Location: /admin/users');
+    exit;
+});
+
+$app->get('/admin/users/:iduser', function($iduser) {
+    User::verifyLogin();
+
+    $user = new User();
+
+    $user->get((int)$iduser);
+
+    $page = new PageAdmin();
+
+    $page->setTpl("users-update", array(
+        "user"=>$user->getValues()
+    ));
+});
+
+$app->post('/admin/users/create', function() {
+
+    User::verifyLogin();
+    
+    $user = new User();
+
+    $_POST["inadmin"] = (isset($_POST['inadmin'])) ? 1 : 0; // se o inadmin estiver marcado é 1 se não é 0
+
+    $_POST['despassword'] = password_hash($_POST["despassword"], PASSWORD_DEFAULT, [
+        "cost"=>12
+    ]);
+
+    $user->setData($_POST);
+
+    $user->save();
+
+    header("Location: /admin/users");
+    
+    exit;
+    
+});
+
+$app->post('/admin/users/:iduser', function($iduser) {
+    User::verifyLogin();
+    
+    $user = new User();
+
+    $_POST["inadmin"] = (isset($_POST['inadmin'])) ? 1 : 0; // se o inadmin estiver marcado é 1 se não é 0
+    
+    $user->get((int)$iduser);
+
+    $user->setData($_POST);
+
+    $user->update();
+
+    header("Location: /admin/users");
+    exit;
+
+});
+
+
+
 
 $app->run();
