3044614cc93790ac30a6ff65009ec89415db5e1f
Luciano Baraúna <baraunaluciano@gmail.com>
2019-08-13 17:05:13 -0300

    finalizado aula 115

--- site.php
+++ site.php
@@ -62,7 +62,55 @@ $app->get("/cart", function() {
 	$cart = Cart::getFromSession();
 	$page = new Page();
 
-	$page->setTpl("cart");
+	$page->setTpl("cart", [
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts()
+	]);
+});
+
+$app->get("/cart/:idproduct/add", function($idproduct) {
+	$product =  new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->addProduct($product);
+
+	$qtd = (isset($_GET['qtd'])) ? (int)$_GET['qtd'] : 1;
+	// Adiciona os produtos de acordo com a quantidade.
+	for ($i=0; $i < $qtd; $i++) {
+		$cart->addProduct($product);
+	}
+
+	header("Location: /cart");
+	exit;
+});
+
+$app->get("/cart/:idproduct/minus", function($idproduct) {
+	$product =  new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProduct($product);
+
+	header("Location: /cart");
+	exit;
+});
+
+$app->get("/cart/:idproduct/remove", function($idproduct) {
+	$product =  new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProduct($product, true);
+
+	header("Location: /cart");
+	exit;
 });
 
 ?>
